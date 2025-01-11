9e6021d3e67a14effd9c2d6db178aed44ad231e6
hyker <hykerguro@gmail.com>
2025-01-11 13:21:05 +0800

    [heartbeat] refactor

--- heartbeat/heartbeat/server.py
+++ heartbeat/heartbeat/server.py
@@ -5,67 +5,15 @@ from typing import Any
 
 from loguru import logger
 
-import confctl.util
 import litter
-from confctl import config
-
-
-class Alert:
-    def lost(self, service: str, severe: int, record: dict[str, Any]):
-        raise NotImplementedError
-
-    def revived(self, service: str, record: dict[str, Any]):
-        raise NotImplementedError
-
-
-class ConsoleAlert(Alert):
-    def __int__(self):
-        self.last_alert = 0.
-
-    def _do_alert(self, message):
-        print(message)
-        self.last_alert = time.time()
-
-    def lost(self, service: str, severe: int, record: dict[str, Any]):
-        message = f"{service} 挂掉了：\n" + \
-                  "\t上次心跳时间：" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(record['last'])) + "\n" + \
-                  f"\t失联计数：{severe}"
-        if severe == 0 or time.time() - self.last_alert > record.get("interval", 3600):
-            record["interval"] = record.get("interval", 3600) * 2
-            self._do_alert(message)
-
-    def revived(self, service: str, record: dict[str, Any]):
-        last = record.get('last', 0)
-        message = f"{service} 上线了：\n" + \
-                  ("\t上次心跳时间：" + time.strftime("%Y-%m-%d %H:%M:%S",
-                                                     time.localtime(last)) + "\n") if last > 0 else ""
-        record["interval"] = 3600
-        self._do_alert(message)
-
-    def __str__(self):
-        return "ConsoleAlert"
-
-
-class NtfyAlert(ConsoleAlert):
-    def __init__(self, host: str, port: int, topic: str):
-        self.host = host
-        self.port = port
-        self.topic = topic
-        litter.connect(host, port)
-        from ntfy.api import TopicPublisher
-        self.lp = TopicPublisher(topic)
-
-    def _do_alert(self, message):
-        self.lp.publish(message)
-        self.last_alert = time.time()
-
-    def __str__(self):
-        return f"NtfyAlert://{self.host}:{self.port}/{self.topic}"
-
+from alert import Alert, ConsoleAlert, NtfyAlert
+from confctl import config, util
 
 NOTE = {}
-ALERTS: list[Alert] = []
+
 _EXEPOOL = ThreadPoolExecutor(max_workers=4)
+
+ALERTS: list[Alert] = []
 
 
 def register_alert(hb_url: str):
@@ -78,7 +26,7 @@ def register_alert(hb_url: str):
         topic = mat.group("topic")
         if not (host and port):
             host, port = config.get("redis/host"), config.get("redis/port")
-        ALERTS.append(NtfyAlert(host, port, topic))
+        ALERTS.append(NtfyAlert(topic, host, port))
     else:
         raise ValueError(f"Unknown alert type: {hb_url}")
 
@@ -126,22 +74,18 @@ def main(host, port, channel, interval, tolerance, eliminate):
 
         NOTE[service] = {"last": time.time(), "cnt": 0, "interval": 3600}
 
-    litter.listen_bg(host, port)
+    litter.listen_bg(host, port, "heartbeat_service")
     logger.info(f"Heartbeat service started at redis://{host}:{port}/{channel}, {interval=}, {tolerance=} "
                 f"with {len(ALERTS)} alerts:" + ", ".join(map(str, ALERTS)))
     period_check(interval, tolerance, eliminate)
 
 
 if __name__ == '__main__':
-    parser = confctl.util.get_argparser()
-    args = parser.parse_args()
-    confctl.util.init_config(args)
-    confctl.util.init_loguru_loggers("heartbeat/logs")
+    util.default_arg_config_loggers("heartbeat/logs")
+    litter.connect(config.get("redis/host"), config.get("redis/port"), "heartbeat_service")
 
     for ac in config.get("heartbeat/alerts"):
         register_alert(ac)
-
-    litter.connect(config.get("redis/host"), config.get("redis/port"))
 
     logger.catch(main)(
         config.get("redis/host"),
